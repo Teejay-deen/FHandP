b33cd0c8150d01703821446c8ff2fbc2287cbe0d
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-05-15 12:35:23 +0100

    nav bar designed added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-import './App.css';
+import "./App.css";
+import Header from "./components/common/Header/Header";
+import { BrowserRouter as Router, Switch, Route,  } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          {/* <Route path="/about">
+              <About />
+            </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }
