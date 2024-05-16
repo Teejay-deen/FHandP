61171b15982e7d63dd31bf8247122959d943a388
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-05-16 09:26:14 +0100

    adding recentcard to the home page

--- src/components/home/Featured/Featured.jsx
+++ src/components/home/Featured/Featured.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./featured.css"
 import Heading from '../../common/Heading'
 import FeaturedCard from './FeaturedCard'
+import NiceFeatured from './NiceFeatured'
 
 const Featured = () => {
   return (
@@ -9,8 +10,8 @@ const Featured = () => {
         <section className='featured background'>
             <div className="container">
                 <Heading title=" Featured Property Types" subtitle="Find All Type of Property." />
-                
                 <FeaturedCard />
+                <NiceFeatured />
             </div>
         </section>
     </div>
