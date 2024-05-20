a90591aba02a76b6c23a58c7414a91aed5339dba
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-05-20 18:27:04 +0100

    change the location of the ifram map

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Contact.css";
 import Back from "../common/Back";
+import Heading from "../common/Heading";
 
 const Contact = () => {
   return (
@@ -29,21 +30,23 @@ const Contact = () => {
           </form>
         </div>
 
+        <Heading title="Our Office Location" subtitle="search for our Location" />
+
         <div className="map-section">
           <div className="map-frame">
-            
+          
               <iframe
                 width="100%"
-                height="400"
+                height="600"
                 frameborder="0"
                 scrolling="no"
                 marginheight="0"
                 marginwidth="0"
-                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Abuja%20Gwarinpa+(Abuja%20fhandp)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
               >
-                <a href="https://www.gps.ie/">gps trackers</a>
+                <a href="https://www.gps.ie/">gps tracker sport</a>
               </iframe>
-       
+            
           </div>
         </div>
       </section>
