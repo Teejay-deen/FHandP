6a33a44c474e33fee8f152df3d2ca0a0e5448980
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-05-20 18:18:47 +0100

    adding iframe map to the contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -14,21 +14,37 @@ const Contact = () => {
 
         <div className="container">
           <form action="" className="shadow">
-            <h4>
-             Fillup the form
-            </h4>
+            <h4>Fillup the form</h4>
 
             <div>
               <input type="text" placeholder="Name" name="" id="" />
               <input type="email" placeholder="Email" name="" id="" />
             </div>
 
-            <input type="text" placeholder="Subject"/>
+            <input type="text" placeholder="Subject" />
 
-            <textarea name="" cols='30' rows='10 ' id=""></textarea>
+            <textarea name="" cols="30" rows="10 " id=""></textarea>
 
             <button>Submit Request</button>
           </form>
+        </div>
+
+        <div className="map-section">
+          <div className="map-frame">
+            
+              <iframe
+                width="100%"
+                height="400"
+                frameborder="0"
+                scrolling="no"
+                marginheight="0"
+                marginwidth="0"
+                src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=1%20Grafton%20Street,%20Dublin,%20Ireland+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+              >
+                <a href="https://www.gps.ie/">gps trackers</a>
+              </iframe>
+       
+          </div>
         </div>
       </section>
     </div>
