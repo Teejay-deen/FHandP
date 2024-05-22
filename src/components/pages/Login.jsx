f6aa28420c5834b7f657872b7183313fd1fcb4bb
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-05-22 10:13:10 +0100

    login and register page is added

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "../common/Heading";
 import "./Login.css";
 
 const Login = () => {
+  const Login = "Login";
+  const SignUp = "Sign Up";
+
+  const [currState, setCurrState] = useState(SignUp);
   return (
     <div>
       <div className="hero1 shadowLogin ">
@@ -13,18 +17,27 @@ const Login = () => {
 
           <div className="login">
             <form action="" className="shadow">
-              <Heading
-                title="Sign Up"
-                subtitle="Fill in the information  below to get started"
-              />
+              {/* <Heading title={currState} subtitle= 'Fill in the information below to get started' /> */}
+
+              <div className="login-title">
+                <h2>{currState}</h2>
+                <p>Fill in the information below to get started</p>
+              </div>
 
               <div className="input1">
-                <input type="text" placeholder="First name" />
-                <input type="text" placeholder="Last name" />
+                {currState === Login ? (
+                  <></>
+                ) : (
+                  <>
+                    <input type="text" placeholder="First name" />
+                    <input type="text" placeholder="Last name" />
+                    <input type="password" placeholder="Confrm Password" />
+                    <input type="text" placeholder="Referal code" />
+                  </>
+                )}
+
                 <input type="text" placeholder="Email" />
                 <input type="Password" placeholder="Password" />
-                <input type="password" placeholder="Confrm Password" />
-                <input type="text" placeholder="Referal code" />
               </div>
 
               <div className="policy">
@@ -36,11 +49,27 @@ const Login = () => {
               </div>
 
               <div className="btn-flex">
-                <button className="continuebtn">Continue</button>
-                <p className="account">
-                  Already have an account ? <a href="">Sign in</a>
-                </p>
+                <button className="continuebtn">
+                  {currState === SignUp ? "Continue" : "Login"}
+                </button>
+
+                {currState === Login ? (
+                  <p>
+                    Create new account ?{" "}
+                    <span className="span1" onClick={() => setCurrState(SignUp)}>
+                      Click here!
+                    </span>
+                  </p>
+                ) : (
+                  <p>
+                    Already have an account ?{" "}
+                    <span className="span1" onClick={() => setCurrState(Login)}>Login here</span>
+                  </p>
+                  
+                )}
               </div>
+
+
             </form>
           </div>
         </div>
